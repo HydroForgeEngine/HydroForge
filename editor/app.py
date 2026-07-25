0b8f91a8f1ae2c5c21d77587b5de65f2bcae9c8d
HydroForgeEngine <lupazupaengine@gmail.com>
2026-07-25 18:42:53 -0300

    Update app.py

--- editor/app.py
+++ editor/app.py
@@ -1,8 +1,1 @@
-from PySide6.QtWidgets import QMainWindow
 
-class HydroForgeEditor(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("HydroForge Editor")
-        self.resize(1280, 720)
