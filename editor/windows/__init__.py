78a0210c5a191c7bd7e4052062bcecb7061551fc
HydroForgeEngine <lupazupaengine@gmail.com>
2026-07-25 18:41:06 -0300

    Update __init__.py

--- editor/windows/__init__.py
+++ editor/windows/__init__.py
@@ -1,1 +1,21 @@
+from .main_window import MainWindow
+from .scene_view import SceneView
+from .hierarchy import Hierarchy
+from .inspector import Inspector
+from .asset_browser import AssetBrowser
+from .console import Console
+from .project_manager import ProjectManager
+from .settings import EditorSettings
+from .splash import SplashScreen
 
+__all__ = [
+    "MainWindow",
+    "SceneView",
+    "Hierarchy",
+    "Inspector",
+    "AssetBrowser",
+    "Console",
+    "ProjectManager",
+    "EditorSettings",
+    "SplashScreen",
+]
