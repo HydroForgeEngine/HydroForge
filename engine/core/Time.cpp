341f71b75f477df0de192a97d6801fbf32c4f051
HydroForgeEngine <lupazupaengine@gmail.com>
2026-07-26 13:10:36 -0300

    Create Time.cpp

---
+++ engine/core/Time.cpp
@@ -0,0 +1,14 @@
+#include "Time.hpp"
+
+namespace HydroForge
+{
+    float Time::DeltaTime()
+    {
+        return 0.016f;
+    }
+
+    float Time::TotalTime()
+    {
+        return 0.0f;
+    }
+}
