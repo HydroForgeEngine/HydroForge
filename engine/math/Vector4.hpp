cffd4ba3a5490060a437aa57488ae2ad3123b654
HydroForgeEngine <lupazupaengine@gmail.com>
2026-07-26 20:28:12 -0300

    Create Vector4.hpp

---
+++ engine/math/Vector4.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace HydroForge
+{
+    class Vector4
+    {
+    public:
+        float x;
+        float y;
+        float z;
+        float w;
+
+        Vector4();
+        Vector4(float x, float y, float z, float w);
+    };
+}
