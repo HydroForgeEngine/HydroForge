5ace98d868ee67632a099709f7da257ff168409a
HydroForgeEngine <lupazupaengine@gmail.com>
2026-07-26 13:11:22 -0300

    Create Logger.cpp

---
+++ engine/core/Logger.cpp
@@ -0,0 +1,20 @@
+#include "Logger.hpp"
+#include <iostream>
+
+namespace HydroForge
+{
+    void Logger::Info(const std::string& message)
+    {
+        std::cout << "[INFO] " << message << std::endl;
+    }
+
+    void Logger::Warning(const std::string& message)
+    {
+        std::cout << "[WARNING] " << message << std::endl;
+    }
+
+    void Logger::Error(const std::string& message)
+    {
+        std::cerr << "[ERROR] " << message << std::endl;
+    }
+}
