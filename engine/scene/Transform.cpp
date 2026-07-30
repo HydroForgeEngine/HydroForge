8f945ecf60926adf05cd367d53abb826fafad279
HydroForgeEngine <lupazupaengine@gmail.com>
2026-07-30 12:19:29 -0300

    Create Transform.cpp

---
+++ engine/scene/Transform.cpp
@@ -0,0 +1,11 @@
+#include "Transform.hpp"
+
+namespace HydroForge
+{
+    Transform::Transform()
+        : Position(0.0f,0.0f,0.0f),
+          Rotation(0.0f,0.0f,0.0f),
+          Scale(1.0f,1.0f,1.0f)
+    {
+    }
+}
