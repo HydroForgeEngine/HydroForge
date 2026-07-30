61a9e8cfe56587a00772bf807ce757b816e65c92
HydroForgeEngine <lupazupaengine@gmail.com>
2026-07-30 15:42:26 -0300

    Create GamepadButtons.hpp

---
+++ engine/input/GamepadButtons.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace HydroForge
+{
+    enum class GamepadButton
+    {
+        A,
+        B,
+        X,
+        Y,
+
+        Start,
+        Select,
+
+        L1,
+        R1,
+
+        L2,
+        R2,
+
+        L3,
+        R3,
+
+        DPadUp,
+        DPadDown,
+        DPadLeft,
+        DPadRight
+    };
+}
