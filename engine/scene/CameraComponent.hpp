2b51abd5f37c667ef6f396ece523ff589f92269f
HydroForgeEngine <lupazupaengine@gmail.com>
2026-07-30 12:21:58 -0300

    Create CameraComponent.hpp

---
+++ engine/scene/CameraComponent.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace HydroForge
+{
+    class CameraComponent
+    {
+    public:
+        float FOV = 70.0f;
+        float NearPlane = 0.1f;
+        float FarPlane = 1000.0f;
+    };
+}
