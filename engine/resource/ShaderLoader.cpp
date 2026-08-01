2e0c6c127bacc6bb5e5eaaa5c06a18907e5842d1
HydroForgeEngine <lupazupaengine@gmail.com>
2026-08-01 15:29:34 -0300

    Create ShaderLoader.cpp

---
+++ engine/resource/ShaderLoader.cpp
@@ -0,0 +1,13 @@
+#include "ShaderLoader.hpp"
+#include "../core/Logger.hpp"
+
+namespace HydroForge
+{
+    bool ShaderLoader::LoadShader(
+        const std::string& vertex,
+        const std::string& fragment)
+    {
+        Logger::Info("Loading shader.");
+        return true;
+    }
+}
