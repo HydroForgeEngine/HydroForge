a71d3da38a7d5e1747c0ef105f6aa81e0bb8b403
HydroForgeEngine <lupazupaengine@gmail.com>
2026-08-02 10:22:00 -0300

    Create AudioSource.cpp

---
+++ engine/audio/AudioSource.cpp
@@ -0,0 +1,21 @@
+#include "AudioSource.hpp"
+
+namespace HydroForge
+{
+    void AudioSource::SetClip(const AudioClip& clip)
+    {
+        m_Clip = clip;
+    }
+
+    void AudioSource::Play()
+    {
+    }
+
+    void AudioSource::Pause()
+    {
+    }
+
+    void AudioSource::Stop()
+    {
+    }
+}
