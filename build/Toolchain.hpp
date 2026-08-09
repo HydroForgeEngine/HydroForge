8a3d7ea2d1a5281e8f64f4548455073470c0fb21
HydroForgeEngine <lupazupaengine@gmail.com>
2026-08-09 13:56:03 -0300

    Update Toolchain.hpp

--- build/Toolchain.hpp
+++ build/Toolchain.hpp
@@ -22,13 +22,17 @@ namespace HydroForge
         void SetType(ToolchainType type);
         ToolchainType GetType() const;
 
-        void SetPath(const std::string& path);
-        const std::string& GetPath() const;
+        void SetRootPath(const std::string& path);
+        const std::string& GetRootPath() const;
+
+        void SetCompiler(const std::string& compiler);
+        const std::string& GetCompiler() const;
 
         bool IsValid() const;
 
     private:
         ToolchainType m_Type;
-        std::string m_Path;
+        std::string m_RootPath;
+        std::string m_Compiler;
     };
 }
