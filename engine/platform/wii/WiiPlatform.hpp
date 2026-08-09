f6782532da9df1908a350803b8366e8bb43d7d9b
HydroForgeEngine <lupazupaengine@gmail.com>
2026-08-09 13:33:27 -0300

    Create WiiPlatform.hpp

---
+++ engine/platform/wii/WiiPlatform.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../Platform.hpp"
+
+namespace HydroForge
+{
+    class WiiPlatform : public Platform
+    {
+    public:
+        bool Initialize() override;
+        void Update() override;
+        void Shutdown() override;
+
+        PlatformType GetType() const override;
+        const char* GetName() const override;
+
+        int GetScreenWidth() const override;
+        int GetScreenHeight() const override;
+    };
+}
