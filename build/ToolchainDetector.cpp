f5c42177e5bef1666ef406d760e4262613b3336d
HydroForgeEngine <lupazupaengine@gmail.com>
2026-08-09 14:01:28 -0300

    Update ToolchainDetector.cpp

--- build/ToolchainDetector.cpp
+++ build/ToolchainDetector.cpp
@@ -1,35 +1,82 @@
 #include "ToolchainDetector.hpp"
 
-#ifdef _WIN32
-    #include <cstdlib>
-#endif
+#include <cstdlib>
+#include <filesystem>
 
 namespace HydroForge
 {
+    static bool CommandExists(const std::string& command)
+    {
+#ifdef _WIN32
+        std::string test = "where " + command + " >nul 2>&1";
+#else
+        std::string test = "command -v " + command + " >/dev/null 2>&1";
+#endif
+
+        return std::system(test.c_str()) == 0;
+    }
+
     Toolchain ToolchainDetector::Detect(BuildTarget target)
     {
+        Toolchain toolchain;
+
         switch (target)
         {
             case BuildTarget::Windows:
-                return Toolchain(ToolchainType::MSVC);
+            {
+                if (CommandExists("g++"))
+                {
+                    toolchain = Toolchain(ToolchainType::MinGW);
+                    toolchain.SetCompiler("g++");
+                }
+                else if (CommandExists("cl"))
+                {
+                    toolchain = Toolchain(ToolchainType::MSVC);
+                    toolchain.SetCompiler("cl");
+                }
+
+                break;
+            }
 
             case BuildTarget::Nintendo3DS:
-                return Toolchain(ToolchainType::DevkitARM);
+            {
+                if (CommandExists("arm-none-eabi-g++"))
+                {
+                    toolchain = Toolchain(ToolchainType::DevkitARM);
+                    toolchain.SetCompiler("arm-none-eabi-g++");
+                }
+
+                break;
+            }
 
             case BuildTarget::Wii:
-                return Toolchain(ToolchainType::DevkitPPC);
+            {
+                if (CommandExists("powerpc-eabi-g++"))
+                {
+                    toolchain = Toolchain(ToolchainType::DevkitPPC);
+                    toolchain.SetCompiler("powerpc-eabi-g++");
+                }
+
+                break;
+            }
 
             case BuildTarget::PSP:
-                return Toolchain(ToolchainType::PSPDev);
+            {
+                if (CommandExists("psp-g++"))
+                {
+                    toolchain = Toolchain(ToolchainType::PSPDev);
+                    toolchain.SetCompiler("psp-g++");
+                }
+
+                break;
+            }
         }
 
-        return Toolchain();
+        return toolchain;
     }
 
     bool ToolchainDetector::IsInstalled(BuildTarget target)
     {
-        Toolchain toolchain = Detect(target);
-
-        return toolchain.IsValid();
+        return Detect(target).IsValid();
     }
 }
