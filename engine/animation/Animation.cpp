13d6da2c3ebc2f80ec28c6b043a8657134ff43d7
HydroForgeEngine <lupazupaengine@gmail.com>
2026-08-09 10:13:50 -0300

    Create Animation.cpp

---
+++ engine/animation/Animation.cpp
@@ -0,0 +1,10 @@
+#include "Animation.hpp"
+
+namespace HydroForge
+{
+    void Animation::Update(AnimationClip& clip, float deltaTime)
+    {
+        (void)clip;
+        (void)deltaTime;
+    }
+}
